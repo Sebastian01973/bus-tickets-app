04a2038a506a8a359e298d1543f48bdafbb26b8b
Jhon Edwin Acevedo <jhon.acevedo01@uptc.edu.co>
2023-01-10 11:24:52 -0500

    Add endpont role

---
+++ backend/busTickets/ticketsApp/views/roleViewSet.py
@@ -0,0 +1,18 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework import viewsets, permissions
+
+from ticketsApp.models import Role
+from ticketsApp.serializers.roleSerializer import RoleSerializer
+
+
+@extend_schema_view(
+    list=extend_schema(description="List all roles"),
+    retrieve=extend_schema(description="Retrieve a role"),
+    create=extend_schema(description="Create a role"),
+    update=extend_schema(description="Update a role"),
+    destroy=extend_schema(description="Delete a role")
+)
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
