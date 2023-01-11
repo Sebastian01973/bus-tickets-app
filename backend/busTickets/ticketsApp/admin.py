eb69cf1d6d06a56ea3428a0bb1cdbc6282a2a90e
sebastian <sebastian.martinez07@uptc.edu.co>
2023-01-11 15:16:46 -0500

    Bug in create Boxx office

--- backend/busTickets/ticketsApp/admin.py
+++ backend/busTickets/ticketsApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from ticketsApp.models.user import User
+from ticketsApp.models.boxOffice import BoxOffice
+from ticketsApp.models.role import Role
+
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(BoxOffice)
--- backend/busTickets/ticketsApp/urls.py
+++ backend/busTickets/ticketsApp/urls.py
@@ -4,16 +4,17 @@ from rest_framework_extensions import routers
 
 from ticketsApp.views.roleViewSet import RoleViewSet
 from ticketsApp.views.userViewSet import UserViewSet
+from ticketsApp.views.boxOfficeViewSet import BoxOfficeViewSet
 
 router = routers.SimpleRouter()
 router.register(r'role', RoleViewSet)
 router.register(r'user', UserViewSet)
+router.register(r'boxOffice', BoxOfficeViewSet)
 
 urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='ticketsApp:schema'), name='swagger-ui'),
     path('schema/redocs/', SpectacularRedocView.as_view(url_name='ticketsApp:schema'), name='redoc'),
-
 
     path('', include(router.urls)),
 ]
