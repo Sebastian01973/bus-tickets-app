62767b28ef8ce051a4843a90cf4ffcdfcb8f0074
sebastian <sebastian.martinez07@uptc.edu.co>
2023-01-23 11:18:30 -0500

    Add SQL Schema and data and modified payroll

--- backend/busTickets/ticketsApp/models/payroll.py
+++ backend/busTickets/ticketsApp/models/payroll.py
@@ -12,10 +12,10 @@ class Payroll(models.Model):
     id = models.AutoField(primary_key=True, unique=True, null=False)
     date = models.DateField(null=False)
     retention = models.FloatField(null=False)
-    date_start = models.DateField(null=False)
-    date_end = models.DateField()
+    date_start = models.TimeField(null=False)
+    date_end = models.TimeField(null=False)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     boxOffice = models.ForeignKey(BoxOffice, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.id, ' ', self.date, ' ', self.discount, ' ', self.retention, ' ', self.date_start, ' ', self.date_end, ' ', self.vehicle
+        return self.id, ' ', self.date, ' ', self.retention, ' ', self.date_start, ' ', self.date_end, ' ', self.vehicle
